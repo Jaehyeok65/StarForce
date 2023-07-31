58a958b6561771a5298dfbaca1bfb17764b11f3a
Lee, Jaehyeok <dlwogur15@gmail.com>
2023-07-31 22:15:33 +0900

    feat : 스타포스 기능 추가

--- src/router/StarForce.tsx
+++ src/router/StarForce.tsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const StarBack = styled.div`
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
 `;
 
 const Star = styled.div`
     display: grid;
+    grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(9, 30px);
-    grid-gap: 20px;
+    gap: 20px;
     padding-top: 5%;
+    font-size : 15px;
 `;
 
 const StarBtn = styled.button`
@@ -29,6 +31,12 @@ const StarContent = styled.div`
     justify-content: space-aroud;
 `;
 
+const selectlevel = [110, 120, 130, 135, 140, 145, 150, 160, 200, 250];
+
+interface discount {
+    [key: string]: boolean;
+}
+
 const StarForce = () => {
     const [percentage, setPercentage] = useState<number>(30); //현재 강화 확률
     const [current, setCurrent] = useState<number>(0); //현재 강화수치
@@ -36,17 +44,20 @@ const StarForce = () => {
     const [currentmeso, setCurrentmeso] = useState<number>(0); //누적 소모 메소
     const [level, setLevel] = useState<number>(160);
     const [starcatch, setStarcatch] = useState<boolean>(false);
-    const [discount, setDiscount] = useState({
-        피시방 : false,
-        선데이메이플 : false,
-        mvp : false
+    const [discount, setDiscount] = useState<discount>({
+        피시방: false,
+        샤타포스: false,
+        mvp: false,
+        1516: false,
+        30: false,
     });
+    const [spare, setSpare] = useState<number>(0);
     const [chance, setChance] = useState<0 | 1 | 2>(0);
     const [reinforcenum, setReinforcenum] = useState<number>(0);
     const [destroy, setDestroy] = useState<number>(0); //파괴확률
     const [destorynum, setDestroynum] = useState<number>(0); //현재까지 터진 횟수
-    //const [success, setSuccess] = useState<0 | 1 | 2>(0); //0은 성공 1은 실패 2는 파괴
-    const [itemmeso, setItemmeso] = useState<number>(0);
+    const [success, setSuccess] = useState<number>(0); //성공 횟수
+    const [fail, setFail] = useState<number>(0); //실패 횟수
 
     const enforce = (per: number): 0 | 1 | 2 => {
         const num = Math.floor(Math.random() * 100) + 1; // 1 ~ 100까지 난수 생성
@@ -101,10 +112,11 @@ const StarForce = () => {
             //성공했을때
             setCurrent((prev) => prev + 1);
             setChance(0);
+            setSuccess((prev) => prev + 1);
         } else if (success === 1) {
             //실패했을 때
-            if (current > 15) {
-                //15성 이상이라면
+            if (current > 15 && current !== 20) {
+                //15성 이상이며 20성이 아니라면 하락하지 않음
                 setCurrent((prev) => prev - 1);
                 if (chance === 0) {
                     setChance(1);
@@ -112,11 +124,12 @@ const StarForce = () => {
                     setChance(2);
                 }
             }
+            setFail((prev) => prev + 1);
         } else {
             //파괴됐을때 === 자동복구
             setCurrent(12);
             setChance(0);
-            setCurrent((prev) => prev + itemmeso);
+            setCurrentmeso((prev) => prev + spare);
             setDestroynum((prev) => prev + 1);
         }
         setCurrentmeso((prev) => prev + meso);
@@ -139,6 +152,8 @@ const StarForce = () => {
             setDestroy(29.4);
         } else if (current === 24) {
             setDestroy(39.6);
+        } else {
+            setDestroy(0);
         }
     };
 
@@ -164,51 +179,115 @@ const StarForce = () => {
         if (current < 10) {
             return 1000 + (Math.pow(level, 3) * (current + 1)) / 25;
         } else if (current === 10) {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 400
-            );
+            return starforcemeso(current, level, 400);
         } else if (current === 11) {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 220
-            );
+            return starforcemeso(current, level, 270);
         } else if (current === 12) {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 150
-            );
+            return starforcemeso(current, level, 150);
         } else if (current === 13) {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 110
-            );
+            return starforcemeso(current, level, 110);
         } else if (current === 14) {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 75
-            );
+            return starforcemeso(current, level, 75);
         } else {
-            return (
-                1000 + (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / 200
-            );
+            return starforcemeso(current, level, 200);
         }
+    };
+
+    const starforcemeso = (
+        current: number,
+        level: number,
+        denominater: number
+    ): number => {
+        return (
+            1000 +
+            (Math.pow(level, 3) * Math.pow(current + 1, 2.7)) / denominater
+        );
+    };
+
+    const formatting = (param: number): string => {
+        return `${Math.floor(param / 100000000)}억 ${comma(param % 100000000)}`;
+    };
+
+    const comma = (param: number): string => {
+        return param.toLocaleString();
+    };
+
+    const onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { value } = e.target;
+        setLevel(+value);
+    };
+
+    const onSpareChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setSpare(+value);
+    };
+
+    const onCheckChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name } = e.target;
+        setDiscount({
+            ...discount,
+            [name]: !discount[name],
+        });
+    };
+
+    const onStarCatchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setStarcatch((prev) => !prev);
     };
 
     useEffect(() => {
         renewal(current, level);
     }, []);
 
+    console.log(starcatch);
+
     useEffect(() => {
         renewal(current, level);
-    }, [current]);
+    }, [current, level]);
 
     return (
         <StarBack>
             <Star>
-                <div>스타포스 시뮬레이터</div>
                 <div>장비 레벨 선택</div>
+                <div>
+                    <select onChange={onSelectChange}>
+                        {selectlevel.map((item) => (
+                            <option value={item} key={item}>
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>스페어 가격 입력 : </div>
+                <div>
+                    <input type="text" value={spare} onChange={onSpareChange} />
+                </div>
                 <div>현재 강화 수치 : {current}</div>
                 <div>현재 강화 확률 : {percentage}%</div>
+                <div>누적 성공 횟수 : {success}</div>
+                <div>누적 실패 횟수 : {fail}</div>
                 <div>현재 파괴 확률 : {destroy}%</div>
                 <div>현재까지 터진 횟수 : {destorynum}번</div>
-                <div>소모 메소 : {meso}메소</div>
-                <div>누적 소모 메소 : {currentmeso}메소</div>
+                <div>할인 체크</div>
+                <div>
+                    {Object.keys(discount).map((item, index) => (
+                        <label key={index}>
+                            <input
+                                type="checkbox"
+                                name={item}
+                                onChange={onCheckChange}
+                            />
+                            {item}
+                        </label>
+                    ))}
+                </div>
+                <div>스타캐치 : </div>
+                <div>
+                    <label>
+                        <input type="checkbox" onChange={onStarCatchChange} />
+                    </label>
+                </div>
+                <div>소모 메소 : {comma(meso)}메소</div>
+                <div>누적 소모 메소 : {formatting(currentmeso)}메소</div>
                 <div>누적 강화 횟수 : {reinforcenum}번</div>
                 <StarContent>
                     <StarBtn onClick={() => enforceTry(percentage)}>
