af4b08db3fbca7cec1a7cf9b2b39586a8332c8ef
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-09-08 17:09:28 +0900

    feat : API 429 에러에 따른 Fetch 주기 설정

--- src/router/Boss.tsx
+++ src/router/Boss.tsx
@@ -483,6 +483,10 @@ const Boss = () => {
             queryKey: ['characterData', item.ocid],
             queryFn: () =>
                 getCharacterData(item.ocid, moment().format('YYYY-MM-DD')),
+            staleTime: 1000 * 60 * 5,
+            cacheTime: 1000 * 60 * 10,
+            retry : 1,
+            refetchOnWindowFocus: false,
         })),
     });
 
